6ee82b9d5ded8452dbe6d20892af017c6eab7a08
ladyomega <68204412+ladyomega@users.noreply.github.com>
2022-04-08 14:35:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame_ProjectTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
